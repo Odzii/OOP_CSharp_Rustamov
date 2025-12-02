e93dd1c1f6716955440860dec572c8548051de60
Odzii <odzii1702@gmail.com>
2025-12-02 19:33:13 +0700

    feature(Program): ask user age

--- Program.cs
+++ Program.cs
@@ -7,5 +7,12 @@ class Program
         Console.Write("Введите ваше имя: ");
 	var name = Console.ReadLine();
 	Console.WriteLine($"Привет, {name}");
+	
+	Console.Write("Введите ваш возраст: ");
+	var ageInput = Console.ReadLine();
+	if (int.TryParse(ageInput, out int age))
+            Console.WriteLine($"Тебе, {age} лет.");
+	else
+	    Console.WriteLine("Пожалуйста, введите целое число.");
     }
 }
