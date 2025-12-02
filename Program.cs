823f993f619874192ffaa0d2625d061f6e6f6b1a
Odzii <odzii1702@gmail.com>
2025-12-02 19:56:26 +0700

    feature(Program): add reapeat greetings

--- Program.cs
+++ Program.cs
@@ -6,6 +6,19 @@ class Program
     {
         Console.Write("Введите ваше имя: ");
 	var name = Console.ReadLine();
-	Console.WriteLine($"Привет, {name}");
+	Console.Write("Сколько раз поприветствовать вас? ");
+        string countInput = Console.ReadLine();
+
+        if (!int.TryParse(countInput, out int count) || count <= 0)
+        {
+            Console.WriteLine("Введено некорректное число. Буду приветствовать один раз.");
+            count = 1;
+        }
+
+        for (int i = 1; i <= count; i++)
+        {
+            Console.WriteLine($"Привет, {name}!");
+        }
+	
     }
 }
