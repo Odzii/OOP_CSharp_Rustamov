044ab9195c7603a7c7d59be58837e8e879c522ee
Odzii <odzii1702@gmail.com>
2025-12-04 09:44:16 +0700

    feature(lab01): add file lab01

---
+++ labs-csharp/lab01/Model/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace Model
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        Unknown
+    }
+    public class Person
+    {
+
+        private string _name;
+        private string _surname;
+        private int _age;
+        private Gender _gender;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Surname
+        {
+            get { return _surname; }
+            set { _surname = value; }
+        }
+
+        public int Age
+        {
+            get => _age;
+            set => _age = Math.Max(Math.Min(value, 150), 0);
+        }
+
+        public Gender Gender
+        {
+            get => _gender;
+            set => _gender = value;
+        }
+
+        public Person(string name, string surname, int age, Gender gender)
+        {
+            _name = name;
+            _surname = surname;
+            Age =  age;
+            _gender = gender;
+        }
+      
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
