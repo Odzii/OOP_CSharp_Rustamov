db24de11ac2f03b051eba0ff631421d7fd33e4c9
Odzii <odzii1702@gmail.com>
2025-12-04 10:23:13 +0700

    feature(Model): add XML-comments for public methods and attributes

--- labs-csharp/lab01/Model/Person.cs
+++ labs-csharp/lab01/Model/Person.cs
@@ -7,6 +7,10 @@ namespace Model
         Female,
         Unknown
     }
+    /// <summary>
+    /// Представляет человека с именем и фамилией, возрастом и полом.
+    /// </summary>
+
     public class Person
     {
 
@@ -15,30 +19,50 @@ namespace Model
         private int _age;
         private Gender _gender;
 
+        /// <summary>
+        /// Получает или задает имя человека
+        /// </summary>
         public string Name
         {
             get { return _name; }
             set { _name = value; }
         }
 
+        /// <summary>
+        /// Получает или задает фамилию человека
+        /// </summary>
         public string Surname
         {
             get { return _surname; }
             set { _surname = value; }
         }
 
+        /// <summary>
+        /// Получает или задает возраст человека в полных годах.
+        /// Значение ограничивается диапазоном от 0 до 150 включительно.
+        /// </summary>
         public int Age
         {
             get => _age;
             set => _age = Math.Max(Math.Min(value, 150), 0);
         }
 
+        /// <summary>
+        /// Получает или задает пол человека.
+        /// </summary>
         public Gender Gender
         {
             get => _gender;
             set => _gender = value;
         }
 
+        /// <summary>
+        /// Инициализирует новый экземпляр класс <see cref="Person""/>.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="surname"></param>
+        /// <param name="age"></param>
+        /// <param name="gender"></param>
         public Person(string name, string surname, int age, Gender gender)
         {
             _name = name;
@@ -50,7 +74,8 @@ namespace Model
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Person person = new Person("Ruslan", "Rustamov", 1600, Gender.Male);
+            Console.WriteLine(person.Age);
         }
     }
 }
