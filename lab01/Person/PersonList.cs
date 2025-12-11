557ea8cab31514ac0bfc1e3bedb4ec1eaa6f9399
Odzii <odzii1702@gmail.com>
2025-12-11 09:33:43 +0700

    feature(lab01): add Person class with Gender enum
    - add private fields _name, _surname, _age and _gender
    - add public properties to encapsulate these fields
    - add Gender enum inside the model to represent a person's gender

---
+++ lab01/Person/PersonList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class PersonList
+    {
+        /// <summary>
+        /// Внутреннее хранилище элементов.
+        /// </summary>
+        private readonly List<Person> _items;
+    }
+}
--- lab01/Person/Program.cs
+++ lab01/Person/Program.cs
@@ -1,22 +1,33 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
 namespace Person
 {
-    public enum Gender
-    {
-        Male,
-        Female,
-        Unknown
-    }
     /// <summary>
-    /// Представляет человека с именем и фамилией, возрастом и полом.
+    /// Представляет человека с именем и фамилией, возрастом и полом. <see cref="Person"/>
     /// </summary>
 
     public class Person
     {
-
+        /// <summary>
+        /// Имя человека
+        /// </summary>
         private string _name;
+        
+        /// <summary>
+        /// Фамилия человека
+        /// </summary>
         private string _surname;
+        
+        /// <summary>
+        /// Возраст человека
+        /// </summary>
         private int _age;
+        
+        /// <summary>
+        /// Пол человека
+        /// </summary>
         private Gender _gender;
 
         /// <summary>
@@ -24,8 +35,20 @@ namespace Person
         /// </summary>
         public string Name
         {
-            get { return _name; }
-            set { _name = value; }
+            get => _name;
+            set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException(
+                        "Имя не может быть пустым, содержать только пробелы или быть null.",
+                        nameof(value)
+                    );
+
+                if (value.Any(char.IsDigit))
+                    throw new ArgumentException("Имя не может содержать цифр.", nameof(Name));
+                
+                _name = value;
+             }
         }
 
         /// <summary>
@@ -34,7 +57,14 @@ namespace Person
         public string Surname
         {
             get { return _surname; }
-            set { _surname = value; }
+            set {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Фамилия не может быть пустой или Null.", nameof(Surname));
+
+                if (value.Any(char.IsDigit))
+                    throw new ArgumentException("Фамилия не может содержать цифр.", nameof(Surname));
+
+                _surname = value; }
         }
 
         /// <summary>
@@ -44,16 +74,32 @@ namespace Person
         public int Age
         {
             get => _age;
-            set => _age = Math.Max(Math.Min(value, 150), 0);
+            set
+            {
+                const int minAge = 0;
+                const int maxAge = 150;
+                if (value < minAge || value > maxAge)
+                    throw new ArgumentOutOfRangeException(
+                        nameof(Age),
+                        $"Возраст должен быть от {minAge} до {maxAge}."
+                    );
+
+                _age = value;
+            }
         }
 
         /// <summary>
-        /// Получает или задает пол человека.
+        /// Получить или и проверить гендер человека.
         /// </summary>
         public Gender Gender
         {
             get => _gender;
-            set => _gender = value;
+            set
+            {
+                if (value == Gender.Unknown)
+                    throw new ArgumentException("Пол должен быть явно указан Male или Female.", nameof(value));
+                _gender = value;
+            }
         }
 
         /// <summary>
@@ -64,18 +110,18 @@ namespace Person
         /// <param name="age"></param>
         /// <param name="gender"></param>
         public Person(string name, string surname, int age, Gender gender)
-        {
-            _name = name;
-            _surname = surname;
+        {   
+            Name = name;
+            Surname = surname;
             Age = age;
-            _gender = gender;
+            Gender = gender;
         }
 
 
         static void Main(string[] args)
         {
-            Person person = new Person("Ruslan", "Rustamov", 1600, Gender.Male);
-            Console.WriteLine(person.Age);
+            Person person = new Person("Ruslan", "Rustamov", 162222, Gender.Male);
+            Console.WriteLine(person.Gender);
         }
     }
 }
