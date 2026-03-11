d0ce0a9d4da67279e5db3a9ce571ceb416ec69b8
Odzii <odzii1702@gmail.com>
2026-03-12 00:12:58 +0700

    feature(Model): added volume calculation library for sphere, pyramid, and parallelepiped
    - added a common interface for Sphere, Pyramid, and Parallelepiped
    - added the VolumeFigureBase abstract class implementing IVolumeFigure and input validation
    - added classes for volume calculation and object description
    - completed item 4 of the assignment

---
+++ Model/Model/Pyramid.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// Представляет пирамиду 
+    /// и содержит свойства для вычисления площади основания,
+    /// объёма и получения текстового описания фигуры.
+    /// </summary>
+    public class Pyramid : VolumeFigureBase
+    {
+        /// <summary>
+        /// Длина основания пирамиды.
+        /// </summary>
+        private double _baseLength = 0.0;
+
+        /// <summary>
+        /// Ширина основания пирамиды.
+        /// </summary>
+        private double _baseWidth = 0.0;
+
+        /// <summary>
+        /// Высота пирамиды.
+        /// </summary>
+        private double _height = 0.0;
+
+        /// <summary>
+        /// Получает или задаёт длину основания пирамиды.
+        /// Значение должно быть положительным конечным числом.
+        /// </summary>
+        public double BaseLength
+        {
+            get => _baseLength;
+            set
+            {
+                ValidatePositive(value, nameof(BaseLength));
+                _baseLength = value;
+            }
+        }
+
+        /// <summary>
+        /// Получает или задаёт ширину основания пирамиды.
+        /// Значение должно быть положительным конечным числом.
+        /// </summary>
+        public double BaseWidth
+        {
+            get => _baseWidth;
+            set
+            {
+                ValidatePositive(value, nameof(BaseWidth));
+                _baseWidth = value;
+            }
+        }
+
+        /// <summary>
+        /// Получает или задаёт высоту пирамиды.
+        /// Значение должно быть положительным конечным числом.
+        /// </summary>
+        public double Height
+        {
+            get => _height;
+            set
+            {
+                ValidatePositive(value, nameof(Height));
+                _height = value;
+            }
+        }
+
+        /// <summary>
+        /// Получает площадь основания пирамиды.
+        /// </summary>
+        public double BaseArea
+        {
+            get => BaseLength * BaseWidth;
+        }
+
+        /// <summary>
+        /// Получает название типа фигуры.
+        /// </summary>
+        public override string FigureType => "Пирамида";
+
+        /// <summary>
+        /// Получает объём пирамиды.
+        /// </summary>
+        public override double Volume => 1.0 / 2.0 * BaseArea * Height;
+
+        /// <summary>
+        /// Возвращает строковое описание пирамиды
+        /// с указанием её основных параметров.
+        /// </summary>
+        /// <returns>
+        /// Строка с типом фигуры, размерами основания и высотой.
+        /// </returns>
+        public override string GetDescription()
+        {
+            return $"Тип фигуры: {FigureType}" +
+                $"| Ширина основания: {BaseWidth}" +
+                $"| Длина основания: {BaseLength}" +
+                $"| Высота пирамиды: {Height}";
+        }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="Pyramid"/> 
+        /// с указанными размерами.
+        /// </summary>
+        /// <param name="baseLength">Длина основания пирамиды.</param>
+        /// <param name="baseWidth">Ширина основания пирамиды.</param>
+        /// <param name="height">Высота пирамиды.</param>
+        public Pyramid(double baseLength, double baseWidth, double height)
+        {
+            BaseLength = baseLength;
+            BaseWidth = baseWidth;
+            Height = height;
+        }
+    }
+}
---
+++ Model/Model/Sphere.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// Представляет трёхмерную сферу, определяемую своим радиусом. 
+    /// Предоставляет свойства для доступа к радиусу сферы
+    /// и вычисления её объёма.
+    /// </summary>
+    /// <remarks> Объём сферы рассчитывается по формуле (4/3) × π × radius³. 
+    /// Радиус должен быть положительным значением,
+    /// в противном случае при установке свойства будет выброшено исключение.
+    /// </remarks>
+    public class Sphere : VolumeFigureBase
+    {
+        /// <summary>
+        /// Радиус сферы, определяющий её размер. 
+        /// </summary>
+        private double _radius = 0.0;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса Sphere с указанным радиусом.
+        /// </summary>
+        /// <remarks> Если указанный радиус меньше или равен нулю, 
+        /// будет выброшено исключение.</remarks>
+        /// <param name="radius">Радиус сферы. 
+        /// Радиус должен быть положительным значением.</param>
+        public Sphere(double radius)
+        {
+            Radius = radius;
+        }
+
+        /// <summary>
+        /// Получает или задаёт радиус сферы. Радиус должен быть положительным значением.
+        /// </summary>
+        /// <remarks>Установка радиуса в неположительное значение вызывает исключение. 
+        /// Радиус определяет размер сферы и используется в таких расчетах, 
+        /// как площадь и объем.</remarks>
+        public double Radius
+        {
+            get => _radius;
+            set
+            {
+                ValidatePositive(value, nameof(Radius));
+                _radius = value;
+            }
+        }
+
+        /// <summary>
+        /// Получает тип фигуры, представляемой этим экземпляром. 
+        /// В данном случае возвращает строку "Сфера".
+        /// </summary>
+        public override string FigureType => "Сфера";
+
+        /// <summary>
+        /// Получает объем сферы, рассчитанный по стандартной формуле объема сферы.
+        /// </summary>
+        /// <remarks>
+        /// Объем сферы рассчитывается по формуле (4/3) × π × <see cref="Radius"/>³, 
+        /// где <see cref="Radius"/> - это текущий радиус сферы.</remarks>
+        public override double Volume => 
+            4.0 / 3.0 
+            * Math.PI 
+            * Math.Pow(Radius, 3);
+
+        /// <summary>
+        /// Возвращает строку, описывающую фигуру, включая её тип, радиус и объём.
+        /// </summary>
+        /// <returns>
+        /// Форматированная строка, 
+        /// содержащая тип фигуры, радиус и объём,
+        /// при этом объём отображается с точностью до двух десятичных знаков.
+        /// </returns>
+        public override string GetDescription()
+        {
+            return $"Тип фигуры: {FigureType} " +
+                $"| Радиус: {Radius}" +
+                $"| Объём: {Volume:F2}";
+        }
+    }
+}
